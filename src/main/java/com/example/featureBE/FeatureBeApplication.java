3cd43588d844f09ebb0131059132a235278df929
ferozk <feroshkhan2020@gmail.com>
2019-12-13 23:49:23 +0530

    Added new project Structure

---
+++ src/main/java/com/example/featureBE/FeatureBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.featureBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/featureBE/FeatureBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.featureBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
